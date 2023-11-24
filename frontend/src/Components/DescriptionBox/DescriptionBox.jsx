fca219ace6ba382f5121dba95537b1169800443b
satvik9373 <113889674+satvik9373@users.noreply.github.com>
2023-11-24 14:57:31 +0530

    ;

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './DescriptionBox.css'
+ const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className='descriptionbox-navigator'>
+      <div className='descriptionbox-nav-box'>Description</div>
+      <div className='descriptionbox-nav-box fade'>Reviews (0)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>SeekAttire, a beacon of style and substance, transcends the boundaries of fashion by seamlessly blending exceptional quality with affordability on a global stage. With an unwavering commitment to delivering unparalleled products, SeekAttire empowers fashion enthusiasts worldwide to embrace their individuality without compromising on excellence. Elevate your wardrobe with the allure of SeekAttire – where affordability meets uncompromising quality, creating a tapestry of style that knows no borders.</p>
+        <p>
+        At SeekAttire, we celebrate the diversity of style, offering a spectrum of fashion choices for the vibrant Gen-Z community. Embrace self-expression with our collection of baggy and oversized clothing, available in limitless sizes. We believe that fashion should be as unique as you are, and our commitment to inclusivity ensures that everyone can find their perfect fit. Join us in redefining the boundaries of fashion, where individuality knows no size constraints.
+
+
+
+
+
+
+        </p>
+      </div>
+    </div>
+  )
+}
+
+
+export default DescriptionBox;
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 export const Navbar = () => {
     const [menu,setMenu] = useState("shop");
+    const {getTotalCartItems}= useContext(ShopContext);
   return (
     <div className='navbar'>
 <div className="nav-logo">
@@ -20,7 +22,7 @@ export const Navbar = () => {
 <div className="nav-login-cart">
    <Link to='/LoginSignup'><button>Login</button></Link> 
     <Link to='/Cart'><img src={cart_icon} alt="" /></Link>
-    <div className="nav-cart-count">0</div>
+    <div className="nav-cart-count">{getTotalCartItems()}</div>
 </div>
     </div>
   )
